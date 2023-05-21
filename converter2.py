43cb09f2b1c596fdd9b0f4c21765ef751215d92a
Medimuz <pava312@gmail.com>
2023-05-21 11:23:50 +0700

    Add homework

---
+++ converter2.py
@@ -0,0 +1,46 @@
+from onlinerequests import CURRENCIES
+def convert():
+    while True:
+        user_currency = input("Введите имеющуюся валюту: ")
+        if user_currency not in CURRENCIES:
+            print('Такой валюты нет в списке!')
+            continue
+
+        while True:
+            current_amount = float(input('Введите имеющуся сумму: '))
+            if current_amount <= 0:
+                print('Число должно быть больше 0!')
+                continue
+            else: break
+
+        while True:
+            conversion_currency = input('Выберите валюту для конвертации: ')
+            if conversion_currency not in CURRENCIES:
+                print('Такой валюты нет в списке!')
+                continue
+            else:
+                converted_amount = CURRENCIES.get(conversion_currency) / CURRENCIES.get(user_currency) * current_amount
+                break
+
+        return converted_amount, conversion_currency
+
+def main():
+    print('Добро пожаловать в конвертер валют')
+    print('-'*34)
+
+    print('''Наша программа поможет Вам конвертировать валюту.
+    1. Введение имеющейся валюты
+    2. Количество этой валюты
+    3. Выбор валюты для конвертации''')
+    print('-' * 34)
+    print('Вам предложены следующие валюты: ')
+    key_counter = 1
+    for key in CURRENCIES:
+        print(f'{key_counter}. {key}')
+        key_counter += 1
+    print('-' * 34)
+    result = convert()
+    print(f'Итого: {result[0]:.2f} {result[1]}')
+
+if __name__ == '__main__':
+    main()
---
+++ onlinerequests.py
@@ -0,0 +1,11 @@
+import requests
+
+API = 'h1ewiRI78UxdSwTdPr9AJ67MlJHsPi7GjffFIgfI'
+
+HOST = f'https://api.freecurrencyapi.com/v1/latest?apikey={API}&currencies=EUR%2CUSD%2CRUB%2CGBP'
+
+response = requests.get(HOST)
+
+CURRENCIES = response.json()['data']
+
+
